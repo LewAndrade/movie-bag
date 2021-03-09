c0e3838e732f09a4cdca829048b564c488cc327a
Lew <andrade.lew@gmail.com>
2021-03-08 23:38:06 -0300

    Added error handling

--- app.py
+++ app.py
@@ -5,19 +5,20 @@ from flask_jwt_extended import JWTManager
 
 from database.db import initialize_db
 from resources.routes import initialize_routes
+from resources.errors import errors
 
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
 
-api = Api(app)
+api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
-app.config['MONGODB_SETTING'] = {
+app.config['MONGODB_SETTING'] = {  # TODO fix to setting"s"
     'host': 'mongodb://localhost/movie-bag'
 }
 
 initialize_db(app)
 initialize_routes(api)
 
-app.run(debug=True)
+app.run()
--- resources/auth.py
+++ resources/auth.py
@@ -1,32 +1,45 @@
 from flask import request, Response
+from flask_restful import Resource
 from flask_jwt_extended import create_access_token
-
+from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist
 
 from database.models import User
-from flask_restful import Resource
+from resources.errors import SchemaValidationError, EmailAlreadyExistsError, UnauthorizedError, InternalServerError
 
 import datetime
 
 
 class SignupApi(Resource):
     def post(self):
-        body = request.get_json()
-        user = User(**body)
-        user.hash_password()
-        user.save()
-        id = user.id
-        return {'id': str(id)}, 201
+        try:
+            body = request.get_json()
+            user = User(**body)
+            user.hash_password()
+            user.save()
+            id = user.id
+            return {'id': str(id)}, 201
+        except FieldDoesNotExist:
+            raise SchemaValidationError
+        except NotUniqueError:
+            raise EmailAlreadyExistsError
+        except Exception as e:
+            raise InternalServerError
 
 
 class LoginApi(Resource):
     def post(self):
-        body = request.get_json()
-        user = User.objects.get(email=body.get('email'))
-        authorized = user.check_password(body.get('password'))
-        if not authorized:
-            return {'error: invalid email or password'}, 401
+        try:
+            body = request.get_json()
+            user = User.objects.get(email=body.get('email'))
+            authorized = user.check_password(body.get('password'))
+            if not authorized:
+                raise UnauthorizedError
 
-        expires_at = datetime.timedelta(days=7)
-        access_token = create_access_token(
-            identity=str(user.id), expires_delta=expires_at)
-        return {'token': access_token}, 200
+            expires_at = datetime.timedelta(days=7)
+            access_token = create_access_token(
+                identity=str(user.id), expires_delta=expires_at)
+            return {'token': access_token}, 200
+        except (UnauthorizedError, DoesNotExist):
+            raise UnauthorizedError
+        except Exception as e:
+            raise InternalServerError
